a039d76feb7b9c2fa77f67bca47aa0fb02246ae1
yenny <choiwonhee.data@gmail.com>
2022-02-26 13:56:04 +0900

    220226

--- greedy/1931 회의실.py
+++ greedy/1931 회의실.py
@@ -1,3 +1,28 @@
+# n = int(input())
+# times = []
+#
+# for _ in range(n):
+#     start, end = map(int, input().split())
+#     times.append([start, end])
+#
+# times = sorted(times, key=lambda x: x[0])
+# times = sorted(times, key=lambda x: x[1])
+#
+# finish_time = 0
+# count = 0
+#
+# for start, end in times:
+#     if start >= finish_time:
+#         count += 1
+#         finish_time = end
+#
+# print(count)
+
+'''
+LIS - dp (dynamic programming)
+백준 - 전봇대
+'''
+
 n = int(input())
 times = []
 
@@ -5,20 +30,14 @@ for _ in range(n):
     start, end = map(int, input().split())
     times.append([start, end])
 
-times = sorted(times, key=lambda x: x[0])
-times = sorted(times, key=lambda x: x[1])
+time = sorted(times, key=lambda x: (x[0], x[1]))
 
 finish_time = 0
-count = 0
+cnt = 0
 
-for start, end in times:
-    if start >= finish_time:
-        count += 1
+for start, end in time:
+    if start > finish_time:
+        cnt += 1
         finish_time = end
 
-print(count)
-
-'''
-LIS - dp (dynamic programming)
-백준 - 전봇대
-'''
+print(cnt)
