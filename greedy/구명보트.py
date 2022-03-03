de7f22f679596bb0b3c251196fe9e28fb2b3a564
yenny <choiwonhee.data@gmail.com>
2022-03-03 22:45:02 +0900

    220303_greedy보충

--- greedy/구명보트.py
+++ greedy/구명보트.py
@@ -24,29 +24,25 @@ def solution(people, limit):
 
     for i in range(len(people)-1):
         total = people[i] + people[i+1]
+        print(total)
         if total >= 100:
             cnt += 1
         else:
-            # if people[i] > 50:
-            #     cnt += 1
+            if people[0] > 50:
+                cnt += 1
 
     return cnt
-
-
 
 
 def solution(people, limit):
-    answer = 0
-    cnt = 0
-
+    cnt_boat = 0
     people.sort()
+    first, last = 0, len(people) - 1
 
-    start, end = 0, len(people) - 1
+    while first <= last:
+        if people[first] + people[last] <= limit:
+            first += 1
+        last -= 1
+        cnt_boat += 1
 
-    while start <= end:
-        cnt += 1
-        if people[start] + people[end] <= limit:
-            start += 1
-        end -= 1
-
-    return cnt
+    return cnt_boat
