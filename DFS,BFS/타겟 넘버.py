774331e804ad2aa9cb57332a385878805b5ae567
yenny <choiwonhee.data@gmail.com>
2022-03-17 09:35:47 +0900

    220317

--- DFS,BFS/타겟 넘버.py
+++ DFS,BFS/타겟 넘버.py
@@ -16,18 +16,56 @@ def solution(numbers, target):
 
 
 
-def dfs(l, s, numbers, target):
+def dfs(x, y, numbers, target):
     n = len(numbers)
     cnt = 0
-    if l == n:
-        if s == target:
+    if x == n:
+        if y == target:
             return 1
         else:
             return 0
-    cnt += dfs(l+1,s+numbers[l],numbers,target)
-    cnt += dfs(l+1,s-numbers[l],numbers,target)
+    cnt += dfs(x+1, y+numbers[x], numbers, target)
+    cnt += dfs(x+1, y-numbers[x], numbers, target)
     return cnt
 
 def solution(numbers, target):
-    answer = dfs(0,0,numbers,target)
+    answer = dfs(0, 0, numbers, target)
+    return answer
+
+
+
+
+
+
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+# stack을 이용한 dfs여도 마찬가지!
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
     return answer
