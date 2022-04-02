5a7e1d67a5c1b99d10345a7bb6bfca19d808e2fe
yenny <choiwonhee.data@gmail.com>
2022-04-03 00:43:20 +0900

    dp
    [BOJ]9461_파도반수열

--- Dynamic programming/9461 파도반 수열.py
+++ Dynamic programming/9461 파도반 수열.py
@@ -1,1 +1,21 @@
 # https://www.acmicpc.net/problem/9461
+'''
+코드 작성 패턴
+1. 빈리스트 만들기 (입력값에 따른)
+2. 초기값을 설정
+3. 점화식 기반으로 계산값 적용하기
+4. 특정 입력값에 따른 계산값을 리스트에서 추출하기
+'''
+
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+for i in range(1, 98):
+    dp[i+3] = dp[i]+dp[i+1]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
+
